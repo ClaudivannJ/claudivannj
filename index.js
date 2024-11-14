0557491a195ed289a63483755f364b8f0f2d29ab
claudivannj <claudivan.ar2002@gmail.com>
2024-11-14 13:04:04 -0300

    descomentando o service-worker

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-// if ('serviceWorker' in navigator) {
-//     window.addEventListener('load', () => {
-//         navigator.serviceWorker.register('./service-worker.js')
-//             .then((registration) => {
-//                 console.log('ServiceWorker registered: ', registration);
-//             })
-//             .catch((error) => {
-//                 console.error('ServiceWorker registration failed: ', error);
-//             });
-//     });
-// }
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('./service-worker.js')
+            .then((registration) => {
+                console.log('ServiceWorker registered: ', registration);
+            })
+            .catch((error) => {
+                console.error('ServiceWorker registration failed: ', error);
+            });
+    });
+}
 
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.getRegistrations().then(registrations => {
